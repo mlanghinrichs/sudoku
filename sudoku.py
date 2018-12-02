b00b24283600f1cd0bf7c8e23dd2b99a2cc77834
Matt Langhinrichs <mlanghinrichs@gmail.com>
2018-12-02 16:59:41 -0500

    Added second pass

--- sudoku.py
+++ sudoku.py
@@ -9,18 +9,26 @@ class Sudoku():
         self.possibles = [[], [], [], [], [], [], [], [], []]
         self.update()
 
-    def update(self):
-        """Update rows, cols, and squares vars."""
+    def update_dict(self, raw_or_poss="raw"):
+        if raw_or_poss == "raw":
+            pre = ""
+            pull_from = self.raw
+        elif raw_or_poss == "poss":
+            pre = "poss"
+            pull_from = self.possibles
+
         for i in range(9):
-            self.__dict__["row" + str(i)] = self.raw[i]
-            self.__dict__["col" + str(i)] = [row[i] for row in self.raw]
+            self.__dict__[pre + "row" + str(i)] = pull_from[i]
+            self.__dict__[pre + "col" + str(i)] = [row[i] for row in pull_from]
         for a in range(3):
             for b in range(3):
-                self.__dict__[f"sqr{a}{b}"] = []
+                self.__dict__[pre + f"sqr{a}{b}"] = []
         for i in range(9):
             for j in range(9):
                 rbox, cbox = str(i//3), str(j//3)
-                self.__dict__[f"sqr{rbox}{cbox}"].append(self.raw[i][j])
+                self.__dict__[pre + f"sqr{rbox}{cbox}"].append(pull_from[i][j])
+
+    def build_possibles(self):
         self.possibles = [[], [], [], [], [], [], [], [], []]
         for r in range(9):
             for c in range(9):
@@ -35,16 +43,91 @@ class Sudoku():
                         this_box.append(num+1)
                 self.possibles[r].append(this_box)
 
+    def print_possibles(self):
+        print("\n")
+        for line in self.possibles:
+            out = ""
+            for item in line:
+                out += str(item).ljust(16, " ")
+            print(out)
+
+    def update(self):
+        self.update_dict("raw")
+        self.build_possibles()
+        self.update_dict("poss")
+
     def first_pass(self):
+        print("Starting first pass")
         initial = str(self.raw)
         for r in range(9):
             for c in range(9):
                 if len(self.possibles[r][c]) == 1:
-                    print(self.possibles[r][c])
+                    print((r+1, c+1), " -> ", self.possibles[r][c][0])
                     self.raw[r][c] = self.possibles[r][c][0]
-                    print(self.raw[r][c])
         self.update()
+        print("Ending first pass")
         return initial == str(self.raw)
+
+    def second_pass(self):
+        print("Starting second pass")
+        initial = str(self.raw)
+        for r in range(9):
+            for c in range(9):
+                if self.raw[r][c] != 0:
+                    continue
+                self.update()
+                rowmates = self.__dict__["possrow" + str(r)][:c] + self.__dict__["possrow" + str(r)][c+1:] 
+                checkrow = []
+                for fellow in rowmates:
+                    checkrow.extend(fellow)
+                checkrow = set(checkrow)
+
+                colmates = self.__dict__["posscol" + str(c)][:c] + self.__dict__["posscol" + str(c)][c+1:] 
+                checkcol = []
+                for fellow in colmates:
+                    checkcol.extend(fellow)
+                checkcol = set(checkcol)
+
+                pos = (3*(r%3) + (c%3))
+                sqrmates = self.__dict__[f"posssqr{r//3}{c//3}"][:pos] + self.__dict__[f"posssqr{r//3}{c//3}"][pos+1:]
+                checksqr = []
+                for fellow in sqrmates:
+                    checksqr.extend(fellow)
+                checksqr = set(checksqr)
+
+                poss = self.possibles[r][c]
+                for num in range(1, 10):
+                    # Consider only numbers in poss
+                    if not num in poss:
+                        continue
+                    if (not num in checkrow
+                        or not num in checkcol
+                        or not num in checksqr):
+                        self.print_possibles()
+                        print(checkrow,
+                              checkcol,
+                              checksqr)
+                        print("row = " + str(r))
+                        print("col = " + str(c))
+                        print("Pos = " + str(pos))
+                        self.raw[r][c] = num
+                        print((r+1, c+1), " -> ", num)
+                        break
+        self.update()
+        print("Ending second pass")
+        return initial == str(self.raw)
+
+    def solve(self):
+        while True:
+            initial = str(self.raw)
+            while not self.first_pass():
+                pass
+            self.second_pass()
+            if initial == str(self.raw):
+                for line in self.possibles:
+                    print(line)
+                print(self)
+                return
 
     def row(self, *args):
         return self.__dict__["row" + str(args[0])]
@@ -85,28 +168,24 @@ class Sudoku():
             puzz.append(list(nums))
         return cls(puzz)
 
+    @classmethod
+    def process_txt(cls, name):
+        name = "./src/" + name + ".txt"
+        out = []
+        with open(name, "r") as f:
+            temp = f.read().split("\n")
+            for line in temp:
+                line = line.split()
+                line = list(map(int, line))
+                if len(line) != 0:
+                    out.append(line)
+        print(out)
+        return cls(out)
 
-test = Sudoku.random()
-new = Sudoku([[0, 0, 4, 5, 0, 2, 0, 9, 0],
-              [0, 1, 5, 0, 6, 0, 2, 0, 0],
-              [0, 8, 2, 0, 1, 0, 0, 0, 0],
-              [5, 2, 8, 1, 0, 0, 0, 0, 0],
-              [7, 0, 0, 6, 0, 8, 0, 0, 1],
-              [0, 0, 0, 0, 0, 3, 7, 8, 5],
-              [0, 0, 0, 0, 4, 0, 6, 3, 0],
-              [0, 0, 9, 0, 2, 0, 8, 1, 0],
-              [0, 3, 0, 7, 0, 6, 5, 0, 0]])
 
-extreme = Sudoku([[0, 0, 0, 0, 0, 1, 0, 0, 5],
-                  [2, 1, 3, 0, 0, 0, 0, 7, 0],
-                  [0, 0, 5, 0, 7, 0, 4, 2, 0],
-                  [0, 0, 8, 5, 0, 4, 0, 0, 0],
-                  [6, 0, 0, 0, 0, 0, 0, 0, 4],
-                  [0, 0, 0, 7, 0, 9, 6, 0, 0],
-                  [0, 4, 6, 0, 8, 0, 9, 0, 0],
-                  [0, 9, 0, 0, 0, 0, 1, 4, 8],
-                  [3, 0, 0, 4, 0, 0, 0, 0, 0]])
-
-while not (extreme.first_pass()):
-    print(extreme, "\n")
+to_do = Sudoku.process_txt("new_extreme")
+to_do.update()
+to_do.solve()
+# while not (to_do.first_pass()):
+#     print(to_do, "\n")
 
