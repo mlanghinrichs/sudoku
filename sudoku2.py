b033f66144d8b9a752e7367f8537ac200746db62
Matt Langhinrichs <mlanghinrichs@gmail.com>
2018-12-13 00:12:23 -0500

    First version of guesser

--- sudoku2.py
+++ sudoku2.py
@@ -57,7 +57,23 @@ class Sudoku():
         return round((100*len(self)) / 81, 2)
         
     def full(self):
+        """Return True if all cells are full, False if not."""
         return len(self) == 81
+
+    def validate(self):
+        """Returen whether the sudoku values appear to be valid."""
+        valid = True
+        for i in range(9):
+            col_ = [val for val in self.column(i) if val != 0]
+            row_ = [val for val in self.row(i) if val != 0]
+            if len(col_) != len(set(col_)) or len(row_) != len(set(row_)):
+                valid = False
+        for sqx in (0, 3, 6):
+            for sqy in (0, 3, 6):
+                sqr_ = [val for val in self.square(sqx, sqy) if val != 0]
+            if len(sqr_) != len(set(sqr_)):
+                valid = False
+        return valid
 
     def column(self, col_):
         """Iterate over col_'s contents."""
@@ -82,12 +98,14 @@ class Sudoku():
                 yield self[row_, c]
 
     def square(self, row_, col_):
+        """Iterate over the square containing row_, col_."""
         for r in range(9):
             for c in range(9):
                 if r//3 == row_//3 and c//3 == col_//3:
                     yield self[r, c]
 
     def others_in_square(self, row_, col_):
+        """Iterate over the square containing row_, col_ except for that cell."""
         for r in range(9):
             for c in range(9):
                 # if it's in the same square but not the same cell...
@@ -95,9 +113,9 @@ class Sudoku():
                     and not (r == row_ and c == col_)):
                     yield self[r, c]
 
-    def possibles(self, row_, col_):
+    def possibles(self, row_, col_, none_if_full=True):
         """Return set of the possible values for self[row_, col_]."""
-        if self[row_, col_]:
+        if self[row_, col_] and none_if_full:
             return set()
         else:
             poss = set([i+1 for i in range(9)])
@@ -108,15 +126,17 @@ class Sudoku():
             for val in self.others_in_square(row_, col_):
                 poss.discard(val)
             return poss
-    
+
     def other_row_possibles(self, row_, col_):
+        """Return set of possible values for other cells in row."""
         orp = set()
         for c in range(9):
             if c != col_:
                 orp = orp | self.possibles(row_, c)
         return orp
-        
+
     def other_column_possibles(self, row_, col_):
+        """Return set of possible values for other cells in column."""
         ocp = set()
         for r in range(9):
             if r != row_:
@@ -124,6 +144,7 @@ class Sudoku():
         return ocp
     
     def other_square_possibles(self, row_, col_):
+        """Return set of possible values for other cells in square."""
         osp = set()
         for r in range(9):
             for c in range(9):
@@ -132,7 +153,7 @@ class Sudoku():
         return osp
 
     def fill_possibles(self):
-        """Fill cells if they have only one possible value."""
+        """Fill cells if they have only one possible value and return full()."""
         print("db - running fill_possibles()")
         for cell in self:
             r = cell["r"]
@@ -143,19 +164,9 @@ class Sudoku():
                 self[r, c] = val
                 print(f"({r}, {c}) -> {val}")
         return self.full()
-    
-    def fill_possibles_loop(self):
-        return self.loop_function(self.fill_possibles)
-
-    def loop_function(self, func):
-        while True:
-            current = len(self)
-            func()
-            if current == len(self):
-                return self.full()
 
     def only_possibles(self):
-        """Fill cell if it holds the only # possibility in its row/col/sqr."""
+        """Fill cell if it has a unique possibility, return full()."""
         print("db - running only_possibles()")
         for cell in self:
             r, c = cell["r"], cell["c"]
@@ -170,11 +181,41 @@ class Sudoku():
                     print(f"({r}, {c}) -> {val}")
                     self[r, c] = val
         return self.full()
+
+    def guess_dicts(self):
+        out = []
+        for cell in self:
+            r, c = cell["r"], cell["c"]
+            if cell["val"] == 0:
+                out.append({"val": cell["val"],
+                            "r": r,
+                            "c": c,
+                            "poss": self.possibles(r, c)
+                            })
+        for dict_ in out:
+            print(dict_)
+        return out
+
+    def guess(self, *dicts):
+        # given {val, r, c, poss} and a self.validate() function
+        if not dicts:
+            return True
+        for cell in dicts:
+            r, c = cell["r"], cell["c"]
+            for p in cell["poss"]:
+                self[r, c] = p
+                print(f"Guessing ({r}, {c}) -> {p}")
+                if self.validate() and self.guess(*dicts[1:]):
+                    return True
+                elif not self.validate():
+                    print(f"{p} was wrong")
+                    continue
+                else:
+                    print("Something went wrong?")
+            return False
      
-    def only_possibles_loop(self):
-        return self.loop_function(self.only_possibles)
-    
     def solve_funcs(self, *args):
+        """Recur args until none of them change self.raw, return full()."""
         current = len(self)
         while not args[0]():
             if current == len(self) and len(args) > 1:
@@ -183,17 +224,19 @@ class Sudoku():
                 return self.full()
             current = len(self)
         return self.full()
-    
+
     def solve(self):
-        if self.solve_funcs(self.fill_possibles_loop, self.only_possibles_loop):
+        """Call solve_funcs() with current solve algorithms."""
+        if self.solve_funcs(self.fill_possibles, self.only_possibles):
             print("Puzzle complete!")
         else:
-            print("Incomplete.")
+            print("Incomplete. Trying to guess:")
+            print(self.guess(*self.guess_dicts()))
         print(self)
 
     @classmethod
     def process_str(cls, name):
-        """Create a Sudoku from a text file located in ./src/"""
+        """Create a Sudoku from a text file located in ./src/."""
         name = f"./src/{name}.txt"
         with open(name, "r") as f:
             source = f.read().split("\n")
@@ -205,3 +248,4 @@ class Sudoku():
 to_do = Sudoku.process_str("extreme1")
 print(to_do)
 to_do.solve()
+
